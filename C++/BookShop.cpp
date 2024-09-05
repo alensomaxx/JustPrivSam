cad0e8c9b78b9737425c1fa2976476b6dd4b69d4
alensomaxx <alensocreation@gmail.com>
2024-09-05 11:47:03 +0530

    Question Updated

---
+++ C++/BookShop.cpp
@@ -0,0 +1,20 @@
+/* A book shop maintains the inventory of books thar are being sold at the shop.
+ The list includes details such as author, titile , price, publisher and stock position. 
+ Whenever a customer wants a book, the sale person inputs the title and aithor and the system 
+searches the list and displays whether it is available or not. if it is not, 
+an appropriate message is displayed. If it is, then the system displayes 
+the books detials and request for the number of copies required. If the requested 
+copies are avaialble, the total cost of he requested copies is diplayed, otherwise the message 
+"Required copies not in stock" is displayed.
+Design a system using a class called Books with suitable member functions and 
+constructors. Use new operator in constructor.
+Use new operator in constructors to allocate memory spcae required"*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
