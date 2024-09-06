8639980f4a2ab25aca4da5bf31bc0ebaadb43547
alensomaxx <alensocreation@gmail.com>
2024-09-06 19:55:11 +0530

    Make it menu driven | add an option to add books

--- C++/BookShop.cpp
+++ C++/BookShop.cpp
@@ -11,10 +11,96 @@ constructors. Use new operator in constructor.
 Use new operator in constructors to allocate memory spcae required"*/
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main(){
+class Books {
+private:
+    char* author;
+    char* title;
+    char* publisher;
+    float price;
+    int stock;
 
+public:
+    // Constructor to initialize book details and allocate memory using new operator
+    Books(const char* a, const char* t, const char* p, float pr, int s) {
+        author = new char[strlen(a) + 1];
+        strcpy(author, a);
 
+        title = new char[strlen(t) + 1];
+        strcpy(title, t);
+
+        publisher = new char[strlen(p) + 1];
+        strcpy(publisher, p);
+
+        price = pr;
+        stock = s;
+    }
+
+    // Destructor to free dynamically allocated memory
+    ~Books() {
+        delete[] author;
+        delete[] title;
+        delete[] publisher;
+    }
+
+    // Function to display book details
+    void showDetails() const {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Publisher: " << publisher << endl;
+        cout << "Price: $" << price << endl;
+        cout << "Stock: " << stock << " copies available" << endl;
+    }
+
+    // Function to check if the book matches the title and author entered by the user
+    bool searchBook(const char* t, const char* a) const {
+        return (strcmp(t, title) == 0 && strcmp(a, author) == 0);
+    }
+
+    // Function to sell the book if sufficient stock is available
+    void sellBook(int copies) {
+        if (stock >= copies) {
+            float totalCost = price * copies;
+            stock -= copies;
+            cout << "Total cost: $" << totalCost << endl;
+        } else {
+            cout << "Required copies not in stock" << endl;
+        }
+    }
+};
+
+int main() {
+    // Creating an array of books with dynamic memory allocation
+    Books book1("J.K. Rowling", "Harry Potter", "Bloomsbury", 39.99, 50);
+    Books book2("George Orwell", "1984", "Secker & Warburg", 19.99, 30);
+    
+    // Input from the customer
+    char searchTitle[50], searchAuthor[50];
+    cout << "Enter the title of the book: ";
+    cin.getline(searchTitle, 50);
+    cout << "Enter the author of the book: ";
+    cin.getline(searchAuthor, 50);
+    
+    // Searching and selling books
+    if (book1.searchBook(searchTitle, searchAuthor)) {
+        book1.showDetails();
+        int copies;
+        cout << "Enter number of copies required: ";
+        cin >> copies;
+        book1.sellBook(copies);
+    } else if (book2.searchBook(searchTitle, searchAuthor)) {
+        book2.showDetails();
+        int copies;
+        cout << "Enter number of copies required: ";
+        cin >> copies;
+        book2.sellBook(copies);
+    } else {
+        cout << "Book not available" << endl;
+    }
+    
     return 0;
 }
+
+
