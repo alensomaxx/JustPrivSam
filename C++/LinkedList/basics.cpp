e2d8d4f35aa0eb875e234663d09a5c5285a732c4
alensomaxx <alensocreation@gmail.com>
2024-09-15 00:45:38 +0530

    Folder Change

---
+++ C++/LinkedList/basics.cpp
@@ -0,0 +1,21 @@
+// Linked List is something which can relate to as a chain of data where 
+// each end of the chain we will get the details of the next node
+
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+//struct node *n;
+//n=(struct node*)malloc(sizeof(struct node*));
+
+
+int main(){
+
+    cout<<"Hello world!!!";
+
+    return 0;
+}
