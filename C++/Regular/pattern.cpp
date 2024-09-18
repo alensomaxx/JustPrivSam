79776eff06ff4c7c865942b400d21a4ac64f6307
alensomaxx <alensocreation@gmail.com>
2024-09-18 21:24:15 +0530

    Pattern Got a major update!!

--- C++/Regular/pattern.cpp
+++ C++/Regular/pattern.cpp
@@ -40,7 +40,61 @@ int main() {
             cout << "  ";
         }
         for (int j = 1; j <= i; j++) {
-            cout << "*" << " ";
+            if (i%2==0){
+                cout << j << " ";    
+            }else{
+                cout << "*" << " ";
+            }
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            if (i%2==0){
+                cout << j << " ";    
+            }else{
+                cout << "*" << " ";
+            }
+            
+        }
+        cout << endl;
+    }
+
+    // Print the bottom half of the pattern
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            if (i%2==0){
+                cout << j << " ";    
+            }else{
+                cout << "*" << " ";
+            }
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            if (i%2==0){
+                cout << j << " ";    
+            }else{
+                cout << "*" << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/* THIS CAN BE USED TO PRINT THE NUMBER WHICH IS THE FIRST DESIGN
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    // Print the top half of the pattern
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  ";
+        }
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
         }
         for (int j = i - 1; j >= 1; j--) {
             cout << j << " ";
@@ -63,4 +117,4 @@ int main() {
     }
 
     return 0;
-}
+}*/
