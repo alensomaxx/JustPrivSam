fcc40b5739878941ea168e920a0ad0dfc987a17e
alensomaxx <alensocreation@gmail.com>
2024-09-24 12:11:12 +0530

    Corrected the Error || All set

--- C++/File Manupulation/Inventory.cpp
+++ C++/File Manupulation/Inventory.cpp
@@ -1,57 +1,69 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip> // For setiosflags, setw, and setprecision
 #include <string>
 
 using namespace std;
 
-class INVENTORY{
+class INVENTORY {
     char name[10];
     int code;
     float cost;
 public:
     void readdata(void);
     void writedata(void);
-
 };
 
-void INVENTORY :: readdata(void){
-    cout<<"Enter Name: "; cin>>name;
-    cout<<"Enter Code: "; cin>>code;
-    cout<<"Enter Cost: "; cin>>cost;
-    
+// Function to read data from the user
+void INVENTORY::readdata(void) {
+    cout << "Enter Name: "; 
+    cin >> name;
+    cout << "Enter Code: "; 
+    cin >> code;
+    cout << "Enter Cost: "; 
+    cin >> cost;
 }
 
-void INVENTORY :: writedata(void){
-    cout<< setiosflags(ios::left)
-        << setw(10)<<name
-        << setiosflags(ios::right)
-        << setw(10)<<code
-        << setprecision(2)
-        << setw(10)<< cost
-        <<endl; 
+// Function to write data to the console
+void INVENTORY::writedata(void) {
+    cout << setiosflags(ios::left)
+         << setw(10) << name
+         << resetiosflags(ios::left) // Reset left alignment
+         << setiosflags(ios::right)
+         << setw(10) << code
+         << setw(10) << fixed << setprecision(2) << cost
+         << endl;
 }
 
-int main(){
+int main() {
     INVENTORY item[3];
     fstream file;
 
-    file.open("STOCK.DAT", ios::in | ios::out);
-    cout<< "Enter Details for three items \n";
-    for(int i=0; i<3; i++){
-        item[i].readdata();
-
-        file.write((char *) & item[i], sizeof(item[i]));
+    // Open file for input and output, truncating existing content
+    file.open("STOCK.DAT", ios::in | ios::out | ios::trunc | ios::binary);
+    
+    if (!file) {
+        cerr << "Error in opening file!" << endl;
+        return 1; // Exit if file fails to open
     }
 
+    // Input data for 3 items
+    cout << "Enter Details for three items\n";
+    for (int i = 0; i < 3; i++) {
+        item[i].readdata();
+        file.write((char*)&item[i], sizeof(item[i]));
+    }
+
+    // Reset file pointer to the beginning for reading
     file.seekg(0);
 
-    cout<< "\nOUTPUT\n\n";
-    for (i=0; i<3; i++){
-        file.read((char*) & item[i], sizeof(item[i]));
+    // Output data from the file
+    cout << "\nOUTPUT\n\n";
+    for (int i = 0; i < 3; i++) {
+        file.read((char*)&item[i], sizeof(item[i]));
         item[i].writedata();
     }
 
     file.close();
     return 0;
-
 }
