ba0878698fd1593b35e77f7f8749b2fe175427d4
alensomaxx <alensocreation@gmail.com>
2024-09-26 11:21:50 +0530

    check check

--- C++/Regular/tester.cpp
+++ C++/Regular/tester.cpp
@@ -2,14 +2,16 @@
 using namespace std;
 
 int main(){
+    int a;
+    cout<<"Enter a number: "; cin>>a;
+    cout<<"This is just to test the Code!"<< a <<endl;
+    cout<<"This CHECK ME OUT just to test the Code!"<<endl;
+    cout<<"This is just to test the Code!"<<endl;
+    cout<<"This is just to test the Code!"<<endl;
+    cout<<"This is just CHECK ME OUT test the Code!"<<endl;
+    cout<<"This is just to test the Code!"<<endl;
 
-    cout<<"This is just to test the Code!"<<endl;
-    cout<<"This is just to test the Code!"<<endl;
-    cout<<"This is just to test the Code!"<<endl;
-    cout<<"This is just to test the Code!"<<endl;
-    cout<<"This is just to test the Code!"<<endl;
-    cout<<"This is just to test the Code!"<<endl;
-    cout<<"This is just to test the Code!"<<endl;
+    
 
     return 0;
 }
