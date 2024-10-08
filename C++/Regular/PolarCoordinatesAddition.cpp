7ea520b748220eb9feeb9247a45d01ffa4a57da3
alensomaxx <alensocreation@gmail.com>
2024-10-08 11:22:35 +0530

    Question Updated! POLAR COORDINATES

---
+++ C++/Regular/PolarCoordinatesAddition.cpp
@@ -0,0 +1,14 @@
+/*Design a class polar which describes a point in the plane 
+using polar coordinates radius and angle. A point in polar 
+coordinates is shown 
+Use the overloaded + operator to add two objects of polar 
+
+Note that we cannot add polar values of two points directly. 
+This requires first the conversion of points into rectangluar co-ordinates, then adding 
+the corresponding rectangluar co-ordinates and finally converting the result back into 
+polar co-ordinates. yoou need to use the following trignometric formulae:
+x=r*cos(a)
+y=r*sin(a)
+a=atan(y/x)
+r=sqrt(x*x + y*y)
+*/
