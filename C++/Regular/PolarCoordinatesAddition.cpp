73e69f45a3c713d48a1457af396d8753fde1bc73
alensomaxx <alensocreation@gmail.com>
2024-10-08 11:43:01 +0530

    Just somewhat done!

--- C++/Regular/PolarCoordinatesAddition.cpp
+++ C++/Regular/PolarCoordinatesAddition.cpp
@@ -6,9 +6,60 @@ Use the overloaded + operator to add two objects of polar
 Note that we cannot add polar values of two points directly. 
 This requires first the conversion of points into rectangluar co-ordinates, then adding 
 the corresponding rectangluar co-ordinates and finally converting the result back into 
-polar co-ordinates. yoou need to use the following trignometric formulae:
+polar co-ordinates. you need to use the following trignometric formulae:
 x=r*cos(a)
 y=r*sin(a)
 a=atan(y/x)
 r=sqrt(x*x + y*y)
+
+
 */
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Polar{
+    private: 
+        double radius;
+        double angle;
+    public:
+        Polar(double radius = 0, double angle=0) : radius(radius),angle(angle){}
+
+        Polar operator+(const Polar& other )const {
+            double x1 = radius*cos(angle);
+            double y1 = radius*sin(angle);
+            double x2 = other.radius*sin(other.angle);
+            double y2 = other.radius*cos(other.angle);
+
+            double xSum = x1 + x2;
+            double ySum = y1 + y2;
+
+            double newRadius = sqrt(xSum * xSum + ySum * ySum);
+            double newAngle = atan2(ySum, xSum);
+
+            return Polar(newRadius, newAngle);
+
+        }
+        void print() const {
+        cout << "Radius: " << radius << ", Angle: " << angle << endl;
+    }
+};
+
+int main(){
+    Polar p1(3, 45);
+    Polar p2(2, 60);
+
+    Polar p3 = p1 + p2;
+
+    cout << "Point 1: ";
+    p1.print();
+
+    cout << "Point 2: ";
+    p2.print();
+
+    cout << "Sum of points: ";
+    p3.print();
+
+    return 0;
+}
