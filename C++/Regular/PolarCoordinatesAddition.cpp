da7eb1a04974c245a4e5d2fe3cb131925dbf4bdc
alensomaxx <alensocreation@gmail.com>
2024-10-08 11:53:19 +0530

    Key Changes: Angle conversion: The input angles are taken in degrees but converted to radians for trigonometric calculations using M_PI / 180.0. When the final result is calculated, the angle is converted back to degrees. Corrected the trigonometric operations: The incorrect use of sin and cos for x2 and y2 has been fixed. Consistent input prompts: The code now clarifies the angle input is in degrees.

--- C++/Regular/PolarCoordinatesAddition.cpp
+++ C++/Regular/PolarCoordinatesAddition.cpp
@@ -13,53 +13,70 @@ a=atan(y/x)
 r=sqrt(x*x + y*y)
 
 
+Test Case 1: 
+Radius = 3, Angle = 45 degrees
+Radius = 2, Angle = 60 degrees
+Expected Output:
+Radius ≈ 4.36, Angle ≈ 51.32 degrees
 */
 
 #include <iostream>
 #include <cmath>
 using namespace std;
 
-class Polar{
-    private: 
-        double radius;
-        double angle;
-    public:
-        Polar(double radius = 0, double angle=0) : radius(radius),angle(angle){}
+class Polar {
+private:
+    double radius;
+    double angle;  // Angle in radians
+public:
+    // Constructor with default parameters
+    Polar(double radius = 0, double angle = 0) : radius(radius), angle(angle) {}
 
-        Polar operator+(const Polar& other )const {
-            double x1 = radius*cos(angle);
-            double y1 = radius*sin(angle);
-            double x2 = other.radius*sin(other.angle);
-            double y2 = other.radius*cos(other.angle);
+    // Overloaded + operator to add two Polar objects
+    Polar operator+(const Polar& other) const {
+        // Convert angles from degrees to radians for calculations
+        double angle1Rad = angle * M_PI / 180.0;
+        double angle2Rad = other.angle * M_PI / 180.0;
 
-            double xSum = x1 + x2;
-            double ySum = y1 + y2;
+        // Convert Polar coordinates to Rectangular (x, y)
+        double x1 = radius * cos(angle1Rad);
+        double y1 = radius * sin(angle1Rad);
+        double x2 = other.radius * cos(angle2Rad);
+        double y2 = other.radius * sin(angle2Rad);
 
-            double newRadius = sqrt(xSum * xSum + ySum * ySum);
-            double newAngle = atan2(ySum, xSum);
+        // Add the rectangular coordinates
+        double xSum = x1 + x2;
+        double ySum = y1 + y2;
 
-            return Polar(newRadius, newAngle);
+        // Convert back to Polar coordinates
+        double newRadius = sqrt(xSum * xSum + ySum * ySum);
+        double newAngle = atan2(ySum, xSum) * 180.0 / M_PI; // Convert radians to degrees
 
-        }
-        void print() const {
-        cout << "Radius: " << radius << ", Angle: " << angle << endl;
+        return Polar(newRadius, newAngle);
+    }
+
+    // Function to print the Polar coordinates
+    void print() const {
+        cout << "Radius: " << radius << ", Angle: " << angle << " degrees" << endl;
     }
 };
 
-int main(){
-    int a , b;
-    int aa,bb;
-    cout<<"Enter the 1st radius:";cin>>a;
-    cout<<"Enter the 1st degree:";cin>>b;
-    cout<<"Enter the 2nd radius:";cin>>aa;
-    cout<<"Enter the 2nd degree:";cin>>bb;
+int main() {
+    double r1, theta1, r2, theta2;
     
+    cout << "Enter the 1st radius: "; cin >> r1;
+    cout << "Enter the 1st angle (in degrees): "; cin >> theta1;
+    cout << "Enter the 2nd radius: "; cin >> r2;
+    cout << "Enter the 2nd angle (in degrees): "; cin >> theta2;
 
-    Polar p1(a, b);
-    Polar p2(aa, bb);
+    // Create two Polar objects
+    Polar p1(r1, theta1);
+    Polar p2(r2, theta2);
 
+    // Add the two Polar objects
     Polar p3 = p1 + p2;
 
+    // Display the results
     cout << "Point 1: ";
     p1.print();
 
