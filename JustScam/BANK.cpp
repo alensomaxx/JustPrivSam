01bd1bf5b20af356a4cf677d786d4dae245ca6af
alensomaxx <alensocreation@gmail.com>
2024-10-21 12:17:35 +0530

    NEW QUESTION!

---
+++ JustScam/BANK.cpp
@@ -0,0 +1,7 @@
+/*Assume that a bank maintains two kinds of accounts for customers, 
+one called as savings account and the other as current account. 
+The savings account provides compound intrest and withdrawl facilities 
+but no cheque book facility. The Curreent account provides cheque book facility 
+but no intrest. Current account holders should also maintain a minimum 
+balanve and if the balance falls below this level, a service charge 
+is imposed.*/
