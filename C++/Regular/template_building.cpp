45637ccab4f9acc6549cdaf0fd3dc31acdb76458
alensomaxx <alensocreation@gmail.com>
2024-11-15 12:56:23 +0530

    there was a conflict with the builtin swap and my custom swap

--- C++/Regular/template_building.cpp
+++ C++/Regular/template_building.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-template <class T>
-void swap(T &x, T &y)
-{
-    T temp = x;
-    x=y;
-    y=temp;
-}
-void fun(int m, int n, float a, float b)
-{
-    cout << "m and n before swap: "<< m << " " << n << "\n";
-    swap(m,n);
-    cout << "m and n after swap: "<< m << " " << n << "\n";
-
-    cout << "a and b before swap: "<< a << " " << b << "\n";
-    swap(a,b);
-    cout << "a and b after swap: "<< a << " " << b << "\n";
-    
+namespace custom {  // Custom namespace to avoid conflict
+    template <class T>
+    void swap(T &x, T &y) {
+        T temp = x;
+        x = y;
+        y = temp;
+    }
 }
 
-int main()
-{
-    fun(100,200,11.22,33.44);
+void fun(int &m, int &n, float a, float b) {
+    cout << "m and n before swap: " << m << " " << n << "\n";
+    custom::swap(m, n);  // Call swap from the custom namespace
+    cout << "m and n after swap: " << m << " " << n << "\n";
+
+    cout << "a and b before swap: " << a << " " << b << "\n";
+    custom::swap(a, b);  // Call swap from the custom namespace
+    cout << "a and b after swap: " << a << " " << b << "\n";
+}
+
+int main() {
+    int m = 100, n = 200;
+    float a = 11.22, b = 33.44;
+    fun(m, n, a, b);
     return 0;
 }
