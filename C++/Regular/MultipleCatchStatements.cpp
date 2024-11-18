7fafa0d1eecb799bfcb6f19bb33bfaaa0c8b3b2e
alensomaxx <alensocreation@gmail.com>
2024-11-18 12:00:15 +0530

    Multiple

---
+++ C++/Regular/MultipleCatchStatements.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void test(int x) {
+    try {
+        if (x == 1) throw x;
+        else if (x == 0) throw 'x';
+        else if (x == -1) throw 1.0;
+        cout << "End of try-block \n";
+    }
+    catch (char c) {
+        cout << "Caught a Character \n";
+    }
+    catch (int m) {
+        cout << "Caught an integer \n";
+    }
+    catch (double d) {
+        cout << "Caught a double \n";
+    }
+    cout << "End of try-catch system \n\n";
+}
+
+int main() {
+    test(1);
+    test(0);
+    test(-1);
+    test(2);
+
+    return 0;
+}
