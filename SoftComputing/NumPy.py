0536fbb95f1e10ed33e98627d18bc72805bb29bf
alensomaxx <alensocreation@gmail.com>
2025-01-16 11:05:53 +0530

    ()

--- SoftComputing/NumPy.py
+++ SoftComputing/NumPy.py
@@ -2,3 +2,11 @@ import numpy as np
 
 arr=np.array([1,2,3,4,5])
 
+print("Orginal Array: ",arr)
+
+arr_add=arr+5
+print("Array after adding 5 to each element: ",arr_add)
+arr_multiply=arr*5
+print("Array after Multiplying 5 to each element: ",arr_multiply)
+
+
