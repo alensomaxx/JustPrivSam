9710399fb938cfb00ef846f5acdd70a1abe829a7
alensomaxx <alensocreation@gmail.com>
2025-01-16 11:14:41 +0530

    basic controls

--- SoftComputing/NumPy.py
+++ SoftComputing/NumPy.py
@@ -9,4 +9,8 @@ print("Array after adding 5 to each element: ",arr_add)
 arr_multiply=arr*5
 print("Array after Multiplying 5 to each element: ",arr_multiply)
 
+arr_sum=np.sum(arr)
+print("Sum of all elements in the array:", arr_sum)
 
+arr_mean=np.mean(arr)
+print("Mean of the array:",arr_mean)
