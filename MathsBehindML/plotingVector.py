84ea01bcee494e9fea88af17d7d37555ff3fea03
alensomaxx <alensocreation@gmail.com>
2025-01-17 21:54:00 +0530

    ()

--- MathsBehindML/plotingVector.py
+++ MathsBehindML/plotingVector.py
@@ -8,13 +8,13 @@ b=2*a
 print (b)
 
 plt.plot(a[::,:1:], a[::, 1::])
-
 plt.show()
-'''
+
+
 c=np.linspace(0, 2*np.pi, 100)
 print(type(c))
 print(c)
-
+'''
 plt.plot(c, np.sin(c))
 plt.plot(c, np.cos(c))
 
