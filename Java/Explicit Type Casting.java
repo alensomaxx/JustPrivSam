6ff15d7224e92c5bd774c07cdb77a6c8bf2f76c4
alensomaxx <alensocreation@gmail.com>
2025-01-21 14:37:20 +0530

    Java also added

---
+++ Java/Explicit Type Casting.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String args[]){
+        double d = 10.0;
+        float f=(float)d;
+        int i = (int)f;
+        
+        System.out.println("Before conversion Double value is : "+d);
+        System.out.println("After conversion float value is : "+f);
+        System.out.println("After conversion Integer value is : "+i);
+               
+    }
+}
---
+++ Java/Implicit type Casting.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String args[]){
+        int i = 10;
+        float f=i;
+        double d =f;
+        
+        System.out.println("Before conversion Integer value is : "+i);
+        System.out.println("After conversion float value is : "+f);
+        System.out.println("After conversion double value is : "+d);
+               
+    }
+}
