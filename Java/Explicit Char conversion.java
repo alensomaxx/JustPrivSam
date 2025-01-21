94d681ac493eee622dd933f0dd7a4d65749cdf32
alensomaxx <alensocreation@gmail.com>
2025-01-21 14:46:28 +0530

    Java

---
+++ Java/Explicit Char conversion.java
@@ -0,0 +1,12 @@
+class Main{
+    public static void main(String args[]){
+        double d = 90.0;
+        char c = (char)d;
+        int i=(int)c;
+        
+        System.out.println("Converting Double to char : "+c);
+        System.out.println("Converting char to int : "+i);
+        
+               
+    }
+}
---
+++ Java/Implicit Char conversion.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String args[]){
+        char c= 'A';
+        int i = c;
+        
+        System.out.println("Converting Char to Int : "+i);
+               
+    }
+}
