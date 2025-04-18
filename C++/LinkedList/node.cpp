9d08eb0e2e30c20262b6d015e6c1cf0644a2ba40
alensomaxx <alensocreation@gmail.com>
2025-04-18 22:22:09 +0530

    ()

---
+++ C++/LinkedList/node.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+struct node{
+    int data;
+    struct node * next;
+};
+
+struct node * n;
+int main(){
+    n=(struct node*)malloc(sizeof(struct node*));
+
+return 0;
+}
