e9505a8b7a49b94f0a5336edebef023dd808158b
alensomaxx <alensocreation@gmail.com>
2025-04-19 10:41:34 +0530

    ()

--- C++/LinkedList/basics.cpp
+++ C++/LinkedList/basics.cpp
@@ -4,20 +4,39 @@
 #include <iostream>
 using namespace std;
 
-struct node{
-    int data;
-    struct node *next;
-};
+int secondLargest(int arr[], int n){
+    if (n<2){
+        return -1;
+    }
 
-//struct node *n;
-//n=(struct node*)malloc(sizeof(struct node*));
+    int largest = arr[0];
+    int sLargest = -1;
+    for (int i=1; i<n; i++){
+        if (arr[i]> largest){
+            sLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] < largest && arr[i] > sLargest){
+            sLargest = arr[i];
+        }
 
+    }
+    return sLargest;
+}
 
 int main(){
+    int size;
+    cin>>size;
+    if (size<= 0){
+        cout << "Invalid array size!"<< endl;
+        return 1;
+    }   
+    int arr[100];
+    for (int i=0; i<size; i++){
+        cin>> arr[i];
+    }
 
-    cout<<"Hello world!!!";
-    cout<<"Hello world!!!";
-    
+    int s_largest = secondLargest(arr, size);
+    cout << "The Second Largest number is : "<< s_largest<< endl;
 
     return 0;
 }
